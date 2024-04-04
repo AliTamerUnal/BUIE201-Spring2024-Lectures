832cf48801aafd12830656ea8745ef1711f9a200
Ali Tamer Ünal <atu@icrontech.com>
2024-04-04 08:30:19 +0300

    updated arrayds. added list

--- arrayds.py
+++ arrayds.py
@@ -1,70 +1,61 @@
 class Array:
     def __init__(self) -> None:
-        self.size = 0
-        self.data = None
+        self._capacity = 0
+        self._size = 0
+        self._data = None
 
     def __getitem__(self, index):
-        if index >= 0 and index < self.size:
-            return self.data[index]
+        if index >= 0 and index < self._size:
+            return self._data[index]
         return None
     
     def __setitem__(self, index, value):
-        if index >= 0 and index < self.size:
-            self.data[index] = value
+        if index >= 0 and index < self._size:
+            self._data[index] = value
       
-    # O(n)
-    def insert(self, value):
-        # CODE REQUIRED
-        # allocate new data O(1)
-        new_size = self.size + 1
-        new_data = [None] * new_size
+    def expand(self):
+        if self._size < self._capacity:
+            return
+        
+        if self._capacity == 0:
+            new_capacity = 5
+        else:
+            new_capacity = self._capacity + (self._capacity // 2)
+        new_data = [None] * new_capacity
+        for i in range(self._capacity):
+            new_data[i] = self._data[i]
+        self._capacity = new_capacity
+        self._data = new_data
 
-        # copy existing elements to the new data O(n)
-        for i in range(self.size):
-            new_data[i] = self.data[i]
 
-        #insert the new item at the end O(1)
-        new_data[self.size] = value
+   # O(n)
+    def insert_back(self, value):
+        self.expand()
+
+        self._data[self._size] = value
 
         # update attributes O(1)
-        self.size = new_size
-        self.data = new_data
+        self._size += 1
 
     # O(n)
     def insert_front(self, value):
-        # allocate new data
-        new_size = self.size + 1
-        new_data = [None] * new_size
+        self.expand()
 
-        # copy existing elements to the new data
-        for i in range(self.size):
-            new_data[i + 1] = self.data[i]
+        for i in range(self._size):
+            self._data[i + 1] = self._data[i]
 
-        #insert the new item at the beginning
-        new_data[0] = value
-
-        # update attributes 
-        self.size = new_size
-        self.data = new_data
-
-    # O(n)
-    # A possible implementation of Python in operator 
-    def find(self, value):
-        for i in range(self.size):
-            if self.data[i] == value:
-                return True
-        return False
-    
+        self._size += 1
+  
     # O(1)
     def count(self):
-        return self.size
+        return self._size
     
     def contains(self, other):
-        if self.size >= other.size:
-            for i in range (self.size - other.size + 1):
+        if self._size >= other.size:
+            for i in range (self._size - other.size + 1):
                 all_same = True
                 for j in range (other.size):
-                    if self.data[i + j] != other.data[j]:
+                    if self._data[i + j] != other.data[j]:
                         all_same = False
                         break
                 if all_same:
@@ -73,23 +64,23 @@ class Array:
 
 
 my_array = Array()
-my_array.insert(4)
-my_array.insert(5)
-my_array.insert(2)
-my_array.insert(1)
+my_array.insert_back(4)
+my_array.insert_back(5)
+my_array.insert_back(2)
+my_array.insert_back(1)
 
 print(my_array[2])
 my_array[2] = 15
 print(my_array[2])
 
 other = Array()
-other.insert(2)
-other.insert(1)
+other.insert_back(2)
+other.insert_back(1)
 
 result1 = my_array.contains(other)
 
 other2 = Array()
-other2.insert(1)
-other2.insert(2)
+other2.insert_back(1)
+other2.insert_back(2)
 
 result2 = my_array.contains(other2)
